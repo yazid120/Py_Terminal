beca8f8b5c7fd0fbf0fad19ccb9fadbaf998dcb6
yazid120 <yazid.ziad2000@gmail.com>
2026-08-24 15:13:24 +0100

    dev gh-page --deploy

--- landing/vite.config.ts
+++ landing/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  base: command === 'serve' ? '/' : '/Py_Terminal/',
+}))
